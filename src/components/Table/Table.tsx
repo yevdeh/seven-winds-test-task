1f52a076fc56b49483a95e361d00a4a67b45cded
yevdeh <evgeniydegtyaryov@gmail.com>
2025-03-08 15:44:26 +0300

    Title

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,11 @@
 import S from "./Table.module.css";
 
 export const Table = () => {
-  return <div className={S.Table}></div>;
+  return (
+    <div className={S.Table}>
+      <div className={S.TitleWrapper}>
+        <h1 className={S.Title}>Строительно-монтажные работы</h1>
+      </div>
+    </div>
+  );
 };
