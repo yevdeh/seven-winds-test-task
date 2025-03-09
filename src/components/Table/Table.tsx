6a263aae80632ff50209c37b2edb9e6324d2831b
yevdeh <evgeniydegtyaryov@gmail.com>
2025-03-09 18:02:37 +0300

    Table with hardcoded data

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,37 @@
+import { data } from "./Table.data";
 import S from "./Table.module.css";
 
 export const Table = () => {
-  return <div className={S.Table}></div>;
+  return (
+    <div className={S.TableWrapper}>
+      <table className={S.Table}>
+        <thead>
+          <tr>
+            <th>Уровень</th>
+            <th>Наименование работ</th>
+            <th>Основная з/п</th>
+            <th>Оборудование</th>
+            <th>Накладные расходы</th>
+            <th>Сметная прибыль</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr key={row.rowName}>
+              <td>
+                <div className={S.IconWrapper} data-level={row.level}>
+                  <img src="/icons/icon-file.svg" alt="" />
+                </div>
+              </td>
+              <td>{row.rowName}</td>
+              <td>{row.salary}</td>
+              <td>{row.equipmentCosts}</td>
+              <td>{row.overheads}</td>
+              <td>{row.estimatedProfit}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
