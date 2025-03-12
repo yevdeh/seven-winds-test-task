a676fbe458ddbfa2f3a3cfe0e44cdabfd93d1da0
yevdeh <evgeniydegtyaryov@gmail.com>
2025-03-12 15:16:12 +0300

    Table: editable cells on double click

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
 import { data } from "./Table.data";
 import S from "./Table.module.css";
 
 export const Table = () => {
+  useEffect(() => {
+    const handleClickOutsideInput = (e: MouseEvent) => {
+      if ((e.target as HTMLElement).hasAttribute("data-input")) return;
+      const activeTr = document.querySelector('tr[data-is-edited="true"]');
+      if (!activeTr) return;
+      (activeTr as HTMLTableRowElement).dataset.isEdited = "false";
+      activeTr.querySelectorAll("[data-input]").forEach((nodeInput) => {
+        (nodeInput as HTMLDivElement).contentEditable = "false";
+      });
+    };
+    document.addEventListener("click", handleClickOutsideInput);
+    return () => {
+      document.removeEventListener("click", handleClickOutsideInput);
+    };
+  }, []);
+
+  const handleInputDoubleClick = (e: React.MouseEvent) => {
+    const tr = (e.target as HTMLElement).closest("tr")!;
+    tr.dataset.isEdited = "true";
+    tr.querySelectorAll("[data-input]").forEach((nodeInput) => {
+      (nodeInput as HTMLDivElement).contentEditable = "plaintext-only";
+    });
+  };
+
   return (
     <div className={S.TableWrapper}>
       <table className={S.Table}>
@@ -25,19 +50,29 @@ export const Table = () => {
                 </div>
               </td>
               <td>
-                <input className={S.Input} disabled name="rowName" value={row.rowName} />
+                <div className={S.Input} onDoubleClick={handleInputDoubleClick} data-input data-name="rowName">
+                  {row.rowName}
+                </div>
               </td>
               <td>
-                <input className={S.Input} disabled name="salary" value={row.salary} />
+                <div className={S.Input} onDoubleClick={handleInputDoubleClick} data-input data-name="salary">
+                  {row.salary}
+                </div>
               </td>
               <td>
-                <input className={S.Input} disabled name="equipmentCosts" value={row.equipmentCosts} />
+                <div className={S.Input} onDoubleClick={handleInputDoubleClick} data-input data-name="equipmentCosts">
+                  {row.equipmentCosts}
+                </div>
               </td>
               <td>
-                <input className={S.Input} disabled name="overheads" value={row.overheads} />
+                <div className={S.Input} onDoubleClick={handleInputDoubleClick} data-input data-name="overheads">
+                  {row.overheads}
+                </div>
               </td>
               <td>
-                <input className={S.Input} disabled name="estimatedProfit" value={row.estimatedProfit} />
+                <div className={S.Input} onDoubleClick={handleInputDoubleClick} data-input data-name="estimatedProfit">
+                  {row.estimatedProfit}
+                </div>
               </td>
             </tr>
           ))}
